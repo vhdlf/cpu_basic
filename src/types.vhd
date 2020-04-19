3ec1dc4bacfc50df536c265ddf163e296c0a49af
wolfram77 <wolfram77@gmail.com>
2020-04-19 19:45:41 +0530

    save

--- src/types.vhd
+++ src/types.vhd
@@ -8,90 +8,88 @@ package types is
 -- we are making a 16-bit cpu
 subtype byte  is std_logic_vector ((8-1)  downto 0);
 subtype word  is std_logic_vector ((16-1) downto 0);
-subtype dword is std_logic_vector ((32-1) downto 0);
-subtype qword is std_logic_vector ((64-1) downto 0);
-
-
--- with 16 registers
-type cpu_registers is array (0 to (16-1)) of word;
 
 
 -- cpu can be in these states
--- type cpu_state is (
---   halted,
---   paused,
---   fetch_addr,
---   fetch_read,
---   decode_byte1,
---   decode_byte2,
---   execute
--- );
-
--- -- you can ask it to run
--- type cpu_state_in is record
---   run: std_logic;
--- end record cpu_state_in;
-
--- -- and it will indicate its status
--- type cpu_state_out is record
---   run:     std_logic;
---   fetch:   std_logic;
---   decode:  std_logic;
---   execute: std_logic;
--- end record cpu_state_out;
+type cpu_state is (
+  state_halted,
+  state_fetch0,
+  state_fetch1,
+  state_decode,
+  state_load0,
+  state_load1,
+  state_store0,
+  state_store1,
+  state_execute
+);
 
 
--- -- input interface
--- type cpu_input is record
---   state: state_in;
---   mem:   memory_in;
--- end record cpu_input;
-
--- -- output interface
--- type cpu_output is record
---   state: cpu_state_out;
---   mem:   cpu_memory_out;
---   reg:   cpu_registers;
--- end record cpu_output;  
-
--- type cpu_internal is record
---   state: cpu_state;
---   reg:   cpu_registers;
--- end record cpu_internal;
-
-
--- type alu_command is (
---   alu_and,
---   alu_or,
---   alu_not,
---   alu_xor,
---   alu_add,
---   alu_sub,
---   alu_mul,
---   alu_div
--- );
+-- and execute following opcodes
+type cpu_opcode is (
+  opcode_ld,
+  opcode_st,
+  opcode_jmp,
+  opcode_jz,
+  opcode_jnz,
+  opcode_jp,
+  opcode_jn,
+  opcode_mov,
+  opcode_and,
+  opcode_or,
+  opcode_not,
+  opcode_xor,
+  opcode_add,
+  opcode_adc,
+  opcode_sub,
+  opcode_sbb,
+  opcode_mul,
+  opcode_imul,
+  opcode_div,
+  opcode_idiv
+);
 
 
--- procedure cpu_registers_reset (
---   signal r: out cpu_registers);
+-- with 16 + 1 (ip) registers
+type cpu_registers is array (0 to (17-1)) of word;
+
+
+-- 8-bit memory interface
+type cpu_memory is record
+  addr: address;
+  dout: byte;
+  din:  byte;
+  wr:   std_logic;
+end record cpu_memory;
+
+
+-- instruction data (internal)
+type cpu_instruction is record
+  op: cpu_opcode;
+  rd: word;
+  rs: word;
+  imm: word;
+end record cpu_instruction;
+
+
+-- fetch data (internal)
+subtype cpu_fetch_data is array (0 to (4-1)) of byte;
+type cpu_fetch is record
+  addr: address;
+  data: cpu_fetch_data;
+  count: integer range 0 to (8-1)
+end record cpu_fetch;
+
+
+-- data (internal)
+type cpu_data is record
+  state: cpu_state;
+  fetch: cpu_fetch;
+  instr: cpu_instruction;
+  regs:  cpu_registers;
+  mem:   cpu_memory;
+end record cpu_data;
 
 end package types;
-
-
-
-package body types is
-
--- procedure cpu_registers_reset (
---   signal r: out cpu_registers) is
--- begin
---   r.r0 <= 0;
---   r.r1 <= 0;
---   r.r2 <= 0;
---   r.r3 <= 0;
---   r.ip <= 0;
--- end procedure cpu_registers_reset;
-
-end package body types;
 
 
 -- Wrote this after following:
